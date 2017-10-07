a694f32c87e487e1affcdd6f7870f19b6a2bfdff
kollie <zikollie@gmail.com>
2017-10-07 11:07:07 +0000

    Initail commit

---
+++ src/components/LibraryyList.js
@@ -0,0 +1,10 @@
+import React, { component } from 'react';
+import { connect } from 'react-redux';
+
+class LibraryList extends component {
+    render() {
+        return;
+    }
+}
+
+export default connect()(LibraryList);
